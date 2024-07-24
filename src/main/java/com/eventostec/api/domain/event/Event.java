0dbdd77892fe71deb8f22e9937b3594afca3f7d1
Thiago Araujo <thiago.dos.araujo@maisunifacisa.com.br>
2024-07-24 18:15:40 -0300

    feat: event and corsconfig

--- src/main/java/com/eventostec/api/domain/event/Event.java
+++ src/main/java/com/eventostec/api/domain/event/Event.java
@@ -1,6 +1,7 @@
 package com.eventostec.api.domain.event;
 
 
+import com.eventostec.api.domain.address.Address;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -32,5 +33,8 @@ public class Event {
     private Boolean remote;
 
     private Date date;
+
+    @OneToOne(mappedBy = "event", cascade = CascadeType.ALL)
+    private Address address;
 
 }
