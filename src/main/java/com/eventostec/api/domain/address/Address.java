30841a0287ff671c730ceb247565560bce0f1421
Thiago Araujo <thiago.dos.araujo@maisunifacisa.com.br>
2024-07-24 18:15:01 -0300

    feat: address

--- src/main/java/com/eventostec/api/domain/address/Address.java
+++ src/main/java/com/eventostec/api/domain/address/Address.java
@@ -2,11 +2,19 @@ package com.eventostec.api.domain.address;
 
 import com.eventostec.api.domain.event.Event;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.UUID;
 
 @Entity
 @Table(name = "address")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address{
 
     @Id
--- src/main/java/com/eventostec/api/repositories/AddressRepository.java
+++ src/main/java/com/eventostec/api/repositories/AddressRepository.java
@@ -2,8 +2,14 @@ package com.eventostec.api.repositories;
 
 import com.eventostec.api.domain.address.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, UUID> {
+
+    public Optional<Address> findByEventId(UUID eventId);
+
 }
